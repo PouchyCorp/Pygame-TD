b9c57283568ce38cd89ceeb30b194d1b405695f1
PouchyCorp <pouchy.l.acarien@gmail.com>
2023-06-20 14:33:23 +0200

    Main with auto pygame installer

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
+import subprocess
+import urllib.request
+
+def connect(host='https://www.geeksforgeeks.org/'):
+    try:
+        urllib.request.urlopen(host)
+        return True
+    except:
+        return False
+  
+  
+def moduleInstaller(module_name):
+    subprocess.run('python -m pip install --upgrade pip')
+
+    p = subprocess.run('pip3 install '+module_name)
+  
+    if(p.returncode == 1 and connect() == False):
+        print("error!! occurred check\nInternet conection")
+  
+    elif(p.returncode == 0):
+        print("It worked", module_name, " is installed")
+  
+    elif(p.returncode == 1 and connect() == True):
+        print("error!! occurred check\nName of module")
+  
+moduleInstaller("pygame")
+
 import pygame
 import random
 import math

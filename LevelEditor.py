db0f9929e176703f23929a37a59542f0315c3fd4
PouchyCorp <pouchy.l.acarien@gmail.com>
2023-07-05 18:29:58 +0200

    level editor for borderLines

--- LevelEditor.py
+++ LevelEditor.py
@@ -6,8 +6,10 @@ WIDTH, HEIGHT = 1000, 1000
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Pouchy's Lore Level Editor")
 
-def draw():
+def draw(rects):
     WIN.fill("white")
+    for rect in rects:
+        pygame.draw.rect(WIN,"red",rect)
     if pygame.font:
         font = pygame.font.Font(None, 36)
         text = font.render("mousPos: " + str(pygame.mouse.get_pos()), 1, (10, 10, 10))
@@ -15,7 +17,41 @@ def draw():
         WIN.blit(text, textpos)
     pygame.display.update()
 
+x_1 = 0
+y_1 = 0
+
+rects = []
+
+def levelEditor(mouseXY):
+    global x_1
+    global y_1
+    if x_1 != 0 and y_1 != 0:
+            print('Uwu')
+            if abs(mouseXY[0]-x_1) > abs(mouseXY[1]-y_1):
+                rect = pygame.Rect(x_1,y_1,abs(mouseXY[0]-x_1),10)
+                rects.append(rect)
+                print(rects)
+                x_1 = 0
+                y_1 = 0
+                return rects
+            else:
+                rect = pygame.Rect(x_1,y_1,10,abs(mouseXY[1]-y_1))
+                rects.append(rect)
+                print(rects)
+                x_1 = 0
+                y_1 = 0
+                return rects
+
+    if x_1 == 0 and y_1 == 0:
+        x_1 = mouseXY[0]
+        y_1 = mouseXY[1]
+        print(x_1,y_1)
+        return x_1,y_1
+    
+    
 while True:
+
+    mouseXY = pygame.mouse.get_pos()
 
     for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -23,8 +59,10 @@ while True:
                 pygame.quit()
 
             if event.type == pygame.MOUSEBUTTONDOWN:
-                print("")
+                levelEditor(mouseXY)
+                
+                
 
 
     
-    draw()
+    draw(rects)
